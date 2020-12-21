c31bcf78177ef696d69a0b22257d09dd423377e6
ali1129js <ali1129js@gmail.com>
2020-12-21 15:33:04 +0100

    resolvers and typeDefs

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,10 @@
+const User = require("../../models/User");
+
+module.exports = {
+  Mutation: {
+    register(_, args, context, info) {
+      // Validate user data
+      // hash password and auth token
+    },
+  },
+};
